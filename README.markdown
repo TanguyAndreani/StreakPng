2bb10eeae48b55963cefaf3614901ecbe31a67d1
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-08-23 15:58:56 +0200

    add cli args

--- README.markdown
+++ README.markdown
@@ -2,10 +2,10 @@
 
 ![](filename.png)
 
-```bash
-$EDITOR sample_streak.csv
-ruby generate_streak.rb
-open filename.png
+```shell
+${EDITOR} sample_streak.csv
+ruby generate_streak.rb sample_streak.csv chart.png
+open chart.png
 ```
 
 Or go read [generate_streak.rb](/generate_streak.rb) to see how you can generate charts with your own data.
--- generate_streak.rb
+++ generate_streak.rb
@@ -6,8 +6,8 @@ include StreakPng
 
 data = StreakData.new
 
-CSV.foreach('sample_streak.csv') do |row|
+CSV.foreach(ARGV[0]) do |row|
   data.add Date.strptime(row[-1],"%Y.%m.%d"), row[0..-2]
 end
 
-StreakChart.new(4, 10, 10, streakdata: data).generate.save('filename.png')
+StreakChart.new(4, 10, 10, streakdata: data).generate.save(ARGV[1])
