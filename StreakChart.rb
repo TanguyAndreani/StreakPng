854a9f168eb19a484049c3fb62743d434f2fba0b
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-08-23 15:43:14 +0200

    add classes for both chart and streak data

---
+++ StreakChart.rb
@@ -0,0 +1,93 @@
+require 'chunky_png'
+require 'date'
+require 'csv'
+
+module Streak
+  DEFAULT_LEVEL_COLORS = [
+      { minCommits: 0, color: '#ebedf0' },
+      { minCommits: 1, color: '#c6e48b' },
+      { minCommits: 9, color: '#7bc96f' },
+      { minCommits: 17, color: '#239a3b' },
+      { minCommits: 26, color: '#196127' }
+  ]
+
+  class StreakChart
+    def initialize margin, width, height, levelcolors: DEFAULT_LEVEL_COLORS, streakdata: nil
+      @levelcolors = levelcolors
+      if streakdata.nil?
+        @streakdata = StreakData.new
+        @streakdata.add Date.today, "Today's task!"
+      else
+        @streakdata = streakdata
+      end
+      @margin = margin
+      @width = width
+      @height = height
+      @png = ChunkyPNG::Image.new((@width + @margin) * 53 + @margin, (@height + @margin) * 7 + @margin, ChunkyPNG::Color::TRANSPARENT)
+      self
+    end
+
+    def save filename
+      @png.save(filename, :interlace => true)
+      self
+    end
+
+    def generate
+      maxdate = Date.today
+
+      mindate = maxdate.prev_year
+      while mindate.cwday != 1
+        mindate = mindate.prev_day
+      end
+
+      x, y = @margin, @margin
+
+      mindate.upto(maxdate) { |date|
+        draw_square x, y, @levelcolors.reduce({ minCommits: -1, color: '#000' }) { |acc, lvl|
+          if @streakdata.fetch_count(date) >= lvl[:minCommits] && lvl[:minCommits] >= acc[:minCommits]
+            lvl
+          else
+            acc
+          end
+        }[:color]
+
+        y += @margin + @height
+
+        if date.cwday == 7
+          y = @margin
+          x += @margin + @width
+        end
+      }
+
+      self
+    end
+
+    private
+
+    def draw_square(x, y, color)
+      x.upto(x+@width) { |x|
+        y.upto(y+@height) { |y|
+          @png[x,y] = ChunkyPNG::Color.from_hex color
+        }
+      }
+    end
+  end
+
+  class StreakData
+    def initialize
+      @bydate = Hash.new { Array.new }
+    end
+
+    def add date, *tags
+      if date.class != Date
+        STDERR.puts "The date should be an instance of the Date class!"
+        return
+      end
+      @bydate[date.to_s] = @bydate[date.to_s] << tags
+    end
+
+    def fetch_count date
+      @bydate[date.to_s].length
+    end
+  end
+end
--- generate_streak.rb
+++ generate_streak.rb
@@ -1,85 +1,12 @@
 require 'chunky_png'
 require 'csv'
 
-levelcolors = [
-    {
-        minCommits: 0,
-        color: '#ebedf0',
-    },
-    {
-        minCommits: 1,
-        color: '#c6e48b',
-    },
-    {
-        minCommits: 9,
-        color: '#7bc96f',
-    },
-    {
-        minCommits: 17,
-        color: '#239a3b',
-    },
-    {
-        minCommits: 26,
-        color: '#196127',
-    },
-]
+load 'StreakChart.rb'
 
-bydate = Hash.new { Array.new }
-
-maxdate = Date.today
-
-mindate = maxdate.prev_year
-while mindate.cwday != 1
-  mindate = mindate.prev_day
-end
+data = Streak::StreakData.new
 
 CSV.foreach('sample_streak.csv') do |row|
-  date = Date.strptime(row[-1],"%Y.%m.%d").to_s
-
-  bydate[date] = bydate[date] << row[0..-2]
+  data.add Date.strptime(row[-1],"%Y.%m.%d"), row[0..-2]
 end
 
-def draw_square(png, x, y, w, h, color)
-  x.upto(x+w) { |x|
-    y.upto(y+h) { |y|
-      png[x,y] = ChunkyPNG::Color.from_hex(color)
-    }
-  }
-end
-
-margin = 4
-x = margin
-y = margin
-w = 10
-h = 10
-
-png = ChunkyPNG::Image.new((w + margin) * 60, (h + margin) * 8, ChunkyPNG::Color::TRANSPARENT)
-
-mindate.upto(maxdate) { |date|
-  key = date.to_s
-
-  daycount = bydate[key].length
-  p bydate[key], key, date, mindate, maxdate
-
-  color = nil
-
-  levelcolors.sort{|a,b|a[:minCommits] <=> b[:minCommits]}.reverse.each { |min|
-    if daycount >= min[:minCommits]
-      color = min[:color]
-      break
-    end
-  }
-
-  p daycount
-
-  draw_square png, x, y, w, h, color
-
-  y += margin + h
-
-  if date.cwday == 7
-    y = margin
-    x += margin + w
-  end
-}
-
-png.save('filename.png', :interlace => true)
+Streak::StreakChart.new(4, 10, 10, streakdata: data).generate.save('filename.png')
